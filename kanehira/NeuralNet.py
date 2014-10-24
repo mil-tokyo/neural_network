36c2aa0ae2dd6c599d24ab8c448e5b0d70c56bf1
kanehira <kanehira@mi.t.u-tokyo.ac.jp>
2014-10-24 15:00:03 +0900

    change neuralnet

--- kanehira/NeuralNet.py
+++ kanehira/NeuralNet.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import time
 
 class FCLayer:
     
@@ -12,7 +13,7 @@ class FCLayer:
             self.activate=lambda x:np.exp(x) / np.sum(np.exp(x)) 
         elif mode == "Hidden":
             self.activate=np.tanh
-        self.activate_div=lambda x:(1-np.tanh(x)**2)
+        self.activate_div=lambda x:1-np.tanh(x)**2
 
     def CalcForward(self,inp):
         self.inp=inp
@@ -28,11 +29,12 @@ class FCLayer:
     def Update(self,eta):
         self.div=np.dot(np.matrix(self.delta).T,np.matrix(self.inp))
         self.W = np.array(self.W - eta * self.div)
-
+        if np.isnan(self.W).any():
+            raise ValueError
 
     def __str__(self):
-        return "the number of element:%d\nmode:%s\n"%(self.h,self.mode)
-
+        return "the number of element:%d\nmode:%s\nW:%s"%(self.h,self.mode,self.W)
+    
         
 class NeuralNetwork:
     
@@ -40,6 +42,7 @@ class NeuralNetwork:
         ## read network setting ##
         layer_settings=setting["layer_settings"]
         self.layer_num=setting["layer_num"]
+        self.eta=setting["eta"]
         
         ## create layer instance and store in list ##
         self.layer_list=[]
@@ -56,71 +59,98 @@ class NeuralNetwork:
             layer=FCLayer(h,W,mode)
             self.layer_list.append(layer)
 
-        for i,l in enumerate(self.layer_list):
-            print "%dth Layer information"%(i+1)
-            print l
-            
-
-    def train(self,x_train,labels,eta):
+    def train(self,x_train,labels):
         ## x_train:train samples (datanum,featuredim), labels:label (datanum,classnum), eta:training coefficiend ##
         datanum=x_train.shape[0]
-        for i in xrange(datanum):
-            x=x_train[i,:]
-            t=labels[i,:]
-
-            print x
-            print "ForwardPropagetion...\n"
-            output=self.ForwardPropagate(x)
+        iteration=5
         
-            print "BackPropagetion...\n"
-            self.BackPropagate(t,output)
+        for j in xrange(iteration):
+            for i in xrange(datanum):
+                print "data: %d/%d"%(i,datanum)
+                x=x_train[i,:]
+                t=labels[i,:]
 
-            print "Update parameters...\n"
-            self.Update(eta)
+                #print "\nForwardPropagetion..."
+                output=self.ForwardPropagate(x)
+        
+                #print "\nBackPropagetion..."
+                self.BackPropagate(t,output)
+
+                #print "\nUpdate parameters..."
+                self.Update()
+                
 
     def predict(self,x_test,classnum):
         datanum=x_test.shape[0]
         result=np.zeros((datanum,classnum))
         for i in xrange(datanum):
             x=x_test[i,:]
-            print "Predictiong...\n"
-            result[i,:]=self.ForwardPropagate(x)
+            #print "\nPredicting..."
+            output=self.ForwardPropagate(x)
+            result[i,np.argmax(output)]=1
+            
         return result
         
     def ForwardPropagate(self,x):
         input=x
         for i,l in enumerate(self.layer_list):
-            print "%dth Layer calculating...\n"%(i+1)
+            #print "%dth Layer calculating..."%(i+1)
             output=l.CalcForward(input)
             input=output
-            print "inp"
-            print input
-
+           
         return output
     
     def BackPropagate(self,t,output):
-        prev_delta=t-output
+        prev_delta=output-t
         for i,l in enumerate(self.layer_list[::-1]):
-            print "%dth Layer calculating...\n"%(self.layer_num-i-1)
+            #print "%dth Layer calculating..."%(self.layer_num-i-1)
             delta=l.CalcBackward(prev_delta)
             prev_delta=delta
             
-    def Update(self,eta):
+    def Update(self):
         for i,l in enumerate(self.layer_list):
-            print "%dth Layer updating...\n"%(i+1)
-            delta=l.Update(eta)
+            #print "%dth Layer updating..."%(i+1)
+            delta=l.Update(self.eta)
+           
+    def __str__(self):
+        ## show network information ##
+        return "\n".join(map(str,self_list))
 
         
 if __name__ == "__main__":
-    setting={"layer_settings":[10,3,5],
-             "layer_num":3}
+
+    ## load dataset and preprocess ##
+    from sklearn.datasets import load_digits
+    from sklearn import preprocessing
+    from sklearn.metrics import classification_report
+    from sklearn.cross_validation import train_test_split
     
+    digits = load_digits()
+    data = preprocessing.normalize(digits.data, norm='l2')
+    lb = preprocessing.LabelBinarizer()
+    label=lb.fit_transform(digits.target)
+
+    inp_dim=data.shape[1]
+    label_dim=label.shape[1]
+    print label
+
+    train_data,test_data,train_label,test_label=train_test_split(data, label, test_size=0.20)
+
+
+    ## network setting ##
+    setting={"layer_settings":[inp_dim,5,label_dim],
+             "layer_num":3,
+             "eta":0.1
+             }
+
+            
+    ## create Network instance ##
     NN=NeuralNetwork(setting)
-    x=np.ones((10,10))
-    labels=np.zeros((10,5))
-    eta=0.1
-    NN.train(x,labels,eta)
-    x_test=np.ones((10,10))
-    result=NN.predict(x_test,5)
-    print result
+
+    ## train Network ##
+    NN.train(train_data,train_label)
+
+    ## predict ##
+    result=NN.predict(test_data,label_dim)
+    print (classification_report(test_label,result))
     
