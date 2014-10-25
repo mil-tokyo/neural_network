e177d07901e6a6fe699a9e8f2a0748e48f426df1
tomoaki-saito <like_kendo_0921@yahoo.co.jp>
2014-10-25 20:42:12 +0900

    reconstruct neural network

--- saito/network/neural_network.py
+++ saito/network/neural_network.py
@@ -19,14 +19,13 @@ class NeuralNetwork(AbstractNeuralNetwork):
         self.rate = 0.05
         self.activate_function = 'sigmoid'
 
-        for i in range(0,len(num_unit_of_each_layer)-1):
+        for i in range(0,len(num_unit_of_each_layer)):
             self.layers.append(layer.fully_connected_layer.FullyConnectedLayer(num_unit_of_each_layer[i], self.activate_function))
+        self.layers[len(num_unit_of_each_layer)-2].is_softmax = True
 
-        self.layers.append(layer.fully_connected_layer.FullyConnectedLayer(num_unit_of_each_layer[i], self.activate_function, is_output=True))
-
-        for i in range(1,len(self.layers)):
-            previous_layer_node = self.layers[i-1].node_num
-            self.layers[i].init_weight(previous_layer_node)
+        for i in range(0,len(self.layers)-1):
+            next_layer_node = self.layers[i+1].node_num
+            self.layers[i].init_weight(next_layer_node)
 
     def load_config(self):
 #        super(ConvolutionalNeuralNetwork, self).test()
@@ -39,14 +38,13 @@ class NeuralNetwork(AbstractNeuralNetwork):
         for i in range(0,len(self.train_labels)):
             if i % 100 == 0:                
                 print str(i)+" images learned"
+
             # go forward
-            input_node = self.train_images[i]            
-            self.layers[0].forward(input_node)
-            for j in range(1,layers_num):
-                self.layers[j].forward(self.layers[j-1].node)
+            self.layers[0].node = self.train_images[i]
+            for j in range(0,layers_num-1):
+                self.layers[j+1].node = self.layers[j].forward()
 
             # go back
-
             out = self.layers[layers_num-1].node
             label = self.train_labels[i]
             self.loss.loss(out,label)
@@ -56,15 +54,14 @@ class NeuralNetwork(AbstractNeuralNetwork):
                 print "error: "+str(total_error/100.0)
                 total_error = 0
             self.layers[layers_num-1].grad = self.loss.grad
+            self.layers[layers_num-1].back()
             
             for j in range(1,layers_num):
-                self.layers[layers_num-j-1].grad = self.layers[layers_num-j].back()
-                self.layers[layers_num-j].dweight = np.array(np.mat(self.layers[layers_num-j].derr).T*np.mat(self.layers[layers_num-j-1].node))
+                self.layers[layers_num-j-1].back(self.layers[layers_num-j].derr)
 
             # update
-            for j in range(1,layers_num):
-                self.layers[j].weight = self.layers[j].weight - self.rate * self.layers[j].dweight
-                self.layers[j].bias = self.layers[j].bias - self.rate * self.layers[j].dbias
+            for j in range(0,layers_num-1):
+                self.layers[j].update(self.rate)
 
     def test(self):
 #        super(ConvolutionalNeuralNetwork, self).test()
@@ -76,10 +73,9 @@ class NeuralNetwork(AbstractNeuralNetwork):
             if i % 100 == 0:
                 print str(i)+" images tested"
             # go forward
-            input_node = self.test_images[i]
-            self.layers[0].forward(input_node)
-            for j in range(1,layers_num):
-                self.layers[j].forward(self.layers[j-1].node)
+            self.layers[0].node = self.test_images[i]
+            for j in range(0,layers_num-1):
+                self.layers[j+1].node = self.layers[j].forward()
 
             out = self.layers[layers_num-1].node
 #            if i % 100 == 0:
