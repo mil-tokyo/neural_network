443ac9aea2ccf7edff1d7c75a0d7a0a01506f50f
Yusuke-Mukuta <mukuta@mi.t.u-tokyo.ac.jp>
2014-10-28 23:37:48 +0900

    modify grad

--- mukuta/grad.py
+++ mukuta/grad.py
@@ -8,18 +8,15 @@ class Gradient:
     """Gradient Descent"""
     def __init__(self):
         self.dnum=0
-        self.grad=None
+        self.grad=0
 
     def append(self,grad):
-        if (self.grad==None):
-            self.grad=grad
-        else:
-            self.grad=self.grad+grad
+        self.grad=self.grad+grad
         self.dnum=self.dnum+1
 
     def update(self,weight):
         newweight=weight-learnparam*self.grad/self.dnum
-        self.grad=None
+        self.grad=0
         self.dnum=0
         return newweight
 
@@ -27,14 +24,12 @@ class AdaGrad(Gradient):
     """Adaptive Gradient"""
     def __init__(self):
         Gradient.__init__(self)
-        self.sumgrad=None
+        self.sumgrad=1
 
     def update(self,weight):
-        if (self.sumgrad==None):
-            self.sumgrad=np.ones(weight.shape)
         newweight=weight-learnparam*self.grad/self.dnum/(self.sumgrad ** 0.5)
         self.sumgrad=self.sumgrad + (self.grad * (1.0/self.dnum))**2
-        self.grad=None
+        self.grad=0
         self.dnum=0
         return newweight
 
