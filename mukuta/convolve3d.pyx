6df91e1f8836ea1b9f3aca6a7bf8a3bad548ba36
Yusuke-Mukuta <mukuta@mi.t.u-tokyo.ac.jp>
2014-11-03 01:16:54 +0900

    parallelize

--- mukuta/convolve3d.pyx
+++ mukuta/convolve3d.pyx
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 cimport numpy as np
+from cython.parallel import prange
 
 def convolve3d(np.ndarray[np.float_t,ndim=3] ivector,np.ndarray[np.float_t,ndim=4] weight, np.int_t imax,np.int_t jmax,np.int_t kmax,np.int_t sj,np.int_t sk):
     cdef int i,j,k,l,pj,pk
@@ -8,7 +9,7 @@ def convolve3d(np.ndarray[np.float_t,ndim=3] ivector,np.ndarray[np.float_t,ndim=
     cdef int pjmax=weight.shape[2]
     cdef int pkmax=weight.shape[3]
     cdef int lmax=ivector.shape[0]
-    for i in range(imax):
+    for i in prange(imax,nogil=True):
         for l in range(lmax):
            for j in range(jmax):
                for k in range(kmax):
--- mukuta/convolveback.pyx
+++ mukuta/convolveback.pyx
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy as np
 cimport numpy as np
+import cython.parallel
 
 def convolveback(np.ndarray[np.float_t,ndim=3] odiff,np.ndarray[np.float_t,ndim=4] weight, np.ndarray[np.float_t,ndim=3] ivector,np.int_t imax,np.int_t jmax,np.int_t kmax,np.int_t sj,np.int_t sk):
 
@@ -13,7 +14,7 @@ def convolveback(np.ndarray[np.float_t,ndim=3] odiff,np.ndarray[np.float_t,ndim=
     cdef np.ndarray[np.float_t,ndim=4] dweight=np.zeros((imax,lmax,pjmax,pkmax))
     cdef np.ndarray[np.float_t,ndim=3] idiff=np.zeros((lmax,ij,ik))
     
-    for i in range(imax):
+    for i in cython.parallel.prange(imax,nogil=True):
         for l in range(lmax):
             for j in range(jmax):
                 for k in range(kmax):
