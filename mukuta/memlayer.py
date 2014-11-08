8aaaf42fb8bedd1e6be19b9addbbd177f6385dde
Yusuke-Mukuta <mukuta@mi.t.u-tokyo.ac.jp>
2014-11-08 23:27:17 +0900

    fix memlayer

--- mukuta/memlayer.py
+++ mukuta/memlayer.py
@@ -7,14 +7,10 @@ sigma=0.1
 
 class MemFCLayer(layer.FCLayer):
     """Full connect layer remembering previous input"""
-    def __init__(self,intputnum,outputnum,tmem,method):
+    def __init__(self,inputnum,outputnum,tmem,method):
         layer.FCLayer.__init__(self,inputnum,outputnum,method)
         self.tmem=tmem
-        self.ivectors=[]
-        self.ovectors=[]
-        for i in range(tmem):
-            self.ivectors.append(np.zeros((inputnum,1)))
-            self.ovectors.append(np.zeros((outputnum,1)))
+        self.memreset()
 
     def forward(self,ivector):
         self.ivectors=self.ivectors[:-1].append(ivector)
@@ -25,4 +21,11 @@ class MemFCLayer(layer.FCLayer):
     def backward(self,odiff,t):
         self.diff.append(np.outer(odiff,self.ivectors[t]))
         return np.dot(self.weight.T,odiff)
+
+    def memreset(self):
+        self.ivectors=[]
+        self.ovectors=[]
+        for i in range(self.tmem):
+            self.ivectors.append(np.zeros((self.i,1)))
+            self.ovectors.append(np.zeros((self.o,1)))
 
