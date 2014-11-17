3d8eecb647c7bd347c6cabd0a860582628010acd
tomoaki-saito <like_kendo_0921@yahoo.co.jp>
2014-11-17 19:10:43 +0900

    tmp

---
+++ saito/network/recurrent_neural_network.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class RecurrentNeuralNetwork(AbstractNeuralNetwork):
+    def __init__(self):
+        print "initializing network"
+        self.word_weight = None
+        self.hidden_weight = None
+        self.out_weight = None
+
+
+
+
+
+    
